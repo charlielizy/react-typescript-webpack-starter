433d58b9823d631067be917dee9a2b6a4a7b7f3b
kelly <kellyguomf@gmail.com>
2021-02-24 11:16:15 +1100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Counter } from './Counter'
+const App = (): JSX.Element => {
+  return (
+    <>
+      <h1>React TypeScript Webpack Starter Template</h1>
+      <Counter />
+    </>
+  )
+}
+
+export { App }
